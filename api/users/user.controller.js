73c1ba78e7b3f848f7ac3f499642906b0583d978
Nisala - Chamodya <113490575+Nisala-Chamodya@users.noreply.github.com>
2024-02-07 10:47:23 +0530

    User Controller & encypt password here

--- api/users/user.controller.js
+++ api/users/user.controller.js
@@ -1,5 +1,24 @@
 const { create } = require("./user.service");
-
+//import bcrypt
+const { genSaltSync, hashSync } = require("bcrypt");
 module.exports = {
-  createUser: (req, res) => {},
+  createUser: (req, res) => {
+    const body = req.body;
+    //password encypt
+    const salt = genSaltSync(10);
+    body.password = hashSync(body.password, salt);
+    create(body, (err, results) => {
+      if (err) {
+        console.log("Create User Error", err);
+        return res.status(500).json({
+          success: 0,
+          message: "Database Connection Error",
+        });
+      }
+      return res.status(200).json({
+        success: 1,
+        data: results,
+      });
+    });
+  },
 };
